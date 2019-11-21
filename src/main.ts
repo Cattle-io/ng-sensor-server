66b055b986533bf5036bb094e781a39183435aa8
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-11-21 16:49:46 -0500

    Update Reset

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .setTitle('Cattle io | API Explorer ')
-    .setDescription('Cattle.io is realtime iot platform for cattle sensors logging')
-    .setVersion('1.0')
-    .addTag('cattle, iot, sensor, arduino, wifi, ch4')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('explorer', app, document);
-
   await app.listen(3000);
 }
-
 bootstrap();
